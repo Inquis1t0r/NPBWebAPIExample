9c05b13cc7dee3d2d3d18f4f6310015f645ab303
inquis1t0r <piotr.kazimierski92@wp.pl>
2019-09-05 20:54:50 +0200

    initial commit: project setup

---
+++ src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/NbpwebapiApplication.java
@@ -0,0 +1,13 @@
+package pl.inquis1t0r.nbpwebapi.fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpwebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbpwebapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/inquis1t0r/nbpwebapi/fetcher/NbpwebapiApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.inquis1t0r.nbpwebapi.fetcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NbpwebapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
