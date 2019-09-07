7b9a7df50c28d0bea273adaa8e3ea7f2eccae937
inquis1t0r <piotr.kazimierski92@wp.pl>
2019-09-07 23:34:05 +0200

    Rates class fields setup with Setters and Getters

--- src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/domain/Rates.java
+++ src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/domain/Rates.java
@@ -7,4 +7,41 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Rates {
+    @JsonProperty("rates")
+    private BigDecimal bid;
+    private BigDecimal ask;
+    private String no;
+    private List<Rates> rates;
+
+    public List<Rates> getRates() {
+        return rates;
+    }
+
+    public void setRates(List<Rates> rates) {
+        this.rates = rates;
+    }
+
+    public BigDecimal getBid() {
+        return bid;
+    }
+
+    public void setBid(BigDecimal bid) {
+        this.bid = bid;
+    }
+
+    public BigDecimal getAsk() {
+        return ask;
+    }
+
+    public void setAsk(BigDecimal ask) {
+        this.ask = ask;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
 }
