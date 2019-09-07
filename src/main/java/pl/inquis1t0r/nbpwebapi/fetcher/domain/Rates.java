733d489aa076d4509a847e2dbb6e9ecb0e924c26
inquis1t0r <piotr.kazimierski92@wp.pl>
2019-09-07 23:31:47 +0200

    initial project structure setup

---
+++ src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/domain/Rates.java
@@ -0,0 +1,10 @@
+package pl.inquis1t0r.nbpwebapi.fetcher.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Rates {
+}
