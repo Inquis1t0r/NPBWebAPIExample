2ee562aaa9bcc597dd029a35653c1316c3a11925
inquis1t0r <piotr.kazimierski92@wp.pl>
2019-09-08 18:51:29 +0200

    WebController update with simple Thymeleaf table of rates

--- src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/NbpwebapiApplication.java
+++ src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/NbpwebapiApplication.java
@@ -2,20 +2,13 @@ package pl.inquis1t0r.nbpwebapi.fetcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class NbpwebapiApplication {
 
-    private static final Logger log=LoggerFactory.getLogger(NbpwebapiApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(NbpwebapiApplication.class, args);
-
-        RestTemplate restTemplate = new RestTemplate();
-
     }
 
 }
--- src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/controller/WebController.java
+++ src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/controller/WebController.java
@@ -1,14 +1,30 @@
 package pl.inquis1t0r.nbpwebapi.fetcher.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import pl.inquis1t0r.nbpwebapi.fetcher.domain.Rates;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+
 
 @Controller
 public class WebController {
-    @RequestMapping("/")
-    //@ResponseBody
-    public String index(){
+    RestTemplate restTemplate = new RestTemplate();
+
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Rates> ratesList = restTemplate.getForObject("http://api.nbp.pl/api/exchangerates/rates/c/usd/2018-02-03/2019-01-31/?format=json", Rates.class).getRatesList();
+        model.addAttribute("ratesList",  ratesList);
         return "index";
     }
+
 }
