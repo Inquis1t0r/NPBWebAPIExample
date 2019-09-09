05fd5ea55c34a2c19b4d20e78e813dfd8857fafc
inquis1t0r <piotr.kazimierski92@wp.pl>
2019-09-09 21:57:55 +0200

    basic input form added

--- src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/controller/WebController.java
+++ src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/controller/WebController.java
@@ -2,10 +2,12 @@ package pl.inquis1t0r.nbpwebapi.fetcher.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import pl.inquis1t0r.nbpwebapi.fetcher.domain.Rates;
 
+import pl.inquis1t0r.nbpwebapi.fetcher.domain.StartTime;
+import pl.inquis1t0r.nbpwebapi.fetcher.domain.Rates;
 
 import java.util.List;
 
@@ -15,13 +17,25 @@ import java.util.List;
 public class WebController {
     RestTemplate restTemplate = new RestTemplate();
 
+    private String format = "/?format=json";
+    private String URL = "http://api.nbp.pl/api/exchangerates/rates/c/usd/";
+    private String dateToday = "2019-09-09";
 
-
-    @GetMapping("/")
-    public String index(Model model) {
-        List<Rates> ratesList = restTemplate.getForObject("http://api.nbp.pl/api/exchangerates/rates/c/usd/2018-02-03/2019-01-31/?format=json", Rates.class).getRatesList();
-        model.addAttribute("ratesList",  ratesList);
-        return "index";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showForm(Model model) {
+        model.addAttribute("message", new StartTime());
+        return "form";
     }
+
+    @PostMapping("/showResult")
+    public String index(@ModelAttribute(value = "messageBody") String messageBody, BindingResult bindingResult, Model model) {
+        System.out.println("messageBody: " + messageBody);
+        List<Rates> ratesList = restTemplate
+                .getForObject(URL+messageBody+"/"+dateToday+format, Rates.class)
+                .getRatesList();
+        model.addAttribute("ratesList",  ratesList);
+        return "result";
+    }
+
 
 }
---
+++ src/main/java/pl/inquis1t0r/nbpwebapi/fetcher/domain/StartTime.java
@@ -0,0 +1,13 @@
+package pl.inquis1t0r.nbpwebapi.fetcher.domain;
+
+public class StartTime {
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+
+    private String messageBody;
+}
